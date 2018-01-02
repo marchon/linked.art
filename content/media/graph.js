2662f816cc53e940e1315301c9bee0898ed5a747
Rob Sanderson <azaroth42@gmail.com>
2018-01-02 09:57:57 -0800

    javascript diagrams

---
+++ content/media/graph.js
@@ -0,0 +1,11 @@
+var graph = {
+	"nodes": [
+		{"id": "object/1", "group": 1},
+		{"id": "object/2", "group": 1},
+		{"id": "person/1", "group": 5},
+	],
+	"links": [
+		{"source": "object/1", "target": "object/2", "value": 2},
+		{"source": "object/1", "target": "person/1", "value": 1}
+	]
+}
